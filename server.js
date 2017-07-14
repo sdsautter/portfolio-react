f7a26c16527bb780dbab732f52f03a61182a0b72
Phil Sautter <psautter@gmail.com>
2017-07-14 12:57:03 -0400

    added bodyparser and passport configuration

--- server.js
+++ server.js
@@ -1,39 +1,60 @@
-const bodyParser = require  ('body-parser'),
-	  cookieParser = require('cookie-parser'),
-	  express = require('express'),
-	  logger = require('morgan'),
-	  methodOverride = require('method-override')
-	  mongoose = require('mongoose'),
-	  path = require('path'),
-	  app = express(),
-	  db = mongoose.connection,
-	  PORT = 4012 || process.env.PORT,
+// Declare Dependencies 
+const bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
+  express = require('express'),
+  session = require('express-session'),
+  logger = require('morgan'),
+  methodOverride = require('method-override'),
+  mongoose = require('mongoose'),
+  passport = require('passport'),
+  path = require('path');
 
+// Declare database models
+require('./user/model');
+require('./gameInstance/model');
+require('./round/model');
 
-
-mongoose.connect('mongodb://localhost/acronauts');
-
-db.on('error', function(err) {
-    console.log(`Connection error: ${err}`);
+// Create Express server
+const app = express();
+const PORT = process.env.PORT || 4012;
+// Define public directory
+app.use(express.static(path.join(__dirname, 'public')));
+// Define BodyParser
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(bodyParser.json());
+// Persistent user sessions - TODO - MOVE TO REDIS  
+app.use(session({
+  secret: process.env.SECRET || 'abc123',
+  key: process.env.KEY || 'xyz',
+  resave: false,
+  saveUninitialized: false,
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+// Load passport strategies
+// require('./user/passport/passport.js')(passport, User);
+// Define Express Routes
+app.get('/', (req, resp) => {
+  resp.sendFile(__dirname + '/public/login.html');
 });
+// require('./user')(app, passport);
+// require('./gameInstance')(app, passport);
+// require('./round')(app, passport);
 
-db.once('open', function() {
-    console.log('Housten, we have a connection!');
+// Connect to the database
+const db = mongoose.connection;
+mongoose.connect(process.env.DATABASE || 'mongodb://localhost/acronauts');
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+
+db.on('error', (err) => {
+  console.log(`Connection error: ${err}`);
 });
-
-
-
-app.use(express.static(path.join(__dirname , 'public')))
-
-
-
-app.get('/', function(req, resp) {
-	resp.sendFile(__dirname + '/public/login.html')
+// Start the Express server when connected
+db.once('open', () => {
+  console.log('Houston, we have a connection!');
+  app.listen(PORT, () => {
+    console.log(`Localhost:${PORT}`);
+  });
 });
-
-
-
-
-app.listen(PORT, () => {
-	console.log(`Localhost:${PORT}`)
-})
