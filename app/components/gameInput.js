cc53171d3e1958b97ba08a7fe073296276679fa7
sdsautter <sdsautter@gmail.com>
2017-07-17 17:25:51 -0400

    Changed up some of the input

--- app/components/gameInput.js
+++ app/components/gameInput.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 
 export default class GameInput extends Component {
+   constructor() {
+       super();
+       this.submitAnswer = this.submitAnswer.bind(this);
+   }
+    
+    submitAnswer(event) {
+        event.preventDefault();
+        console.log(this.playerAnswer.value());
+        console.log("Answer submitted")
+    }
+
     render() {
         return(          
-            <div className="input-group">
-                <input id="playerAnswer" type="text" className="form-control" placeholder="Type Here" />
+            <form className="input-group" onSubmit={this.submitAnswer}>
+                <input ref={(input) => {this.playerAnswer = input}} type="text" className="form-control" required placeholder="Type Answer Here" />
                     <span className="input-group-btn">
                         <button id="answerSubmit" className="btn btn-secondary" type="button">Submit!</button>
                     </span>
-            </div>    
+            </form>    
         )
     }
 }
