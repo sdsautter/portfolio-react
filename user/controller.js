64fe00406c8e2dc229a4dc4b098e5b98e685ba1b
Phil Sautter <psautter@gmail.com>
2017-07-17 22:31:37 -0400

    added isLoggedIn middleware function, fixed formatting and syntax

--- user/controller.js
+++ user/controller.js
@@ -1,29 +1,33 @@
-const mongoose = require('mongoose'),
-      User = mongoose.model('User');
-      bcrypt = require('bcrypt'),
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const bcrypt = require('bcrypt');
 
 exports.createAccount = (data, cb) => {
-    User.findOne({'email': data.email}, (err, users) =>{
-        if(users){
-            return cb(false)
-        } else {    
-            bcrypt.hash(data.password, 10, function(err, hash) {
-                let newUser = new User({
-                    email: data.email,
-                    username: data.username,
-                    password: hash
-                });
-                newUser.save((err, user) => {
-                    if(err){
-                     console.log(err); 
-                     return cb(false);
-                    }
-                    return cb(true);
-                })
-
-            });    
-        } 
-    })
-
+  User.findOne({
+    email: data.email,
+  }, (err, users) => {
+    if (users) return cb(false);
+    bcrypt.hash(data.password, 10, (err, hash) => {
+      let newUser = new User({
+        email: data.email,
+        username: data.username,
+        password: hash,
+      });
+      newUser.save((err, user) => {
+        if (err) {
+          // console.log(err);
+          return cb(false);
+        }
+        return cb(true);
+      });
+    });
+  });
 };
 
+exports.isLoggedIn = (req, res, next) => {
+  // If the user is authenticated continue to the next function
+  if (req.isAuthenticated()) return next();
+  // keep track of where the user is to send back after they login
+  req.session.returnTo = req.path;
+  return res.redirect('/login');
+};
