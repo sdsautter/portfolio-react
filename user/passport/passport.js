dac6d3136bcd33cb4ef8ecb36dc295f1c6b29d73
Phil Sautter <psautter@gmail.com>
2017-07-17 22:29:05 -0400

    code syntax and formatting improved

--- user/passport/passport.js
+++ user/passport/passport.js
@@ -1,44 +1,38 @@
-const bcrypt = require('bcrypt'),
-    LocalStrategy = require('passport-local').Strategy;
-    Users = require('../model.js');
+const bcrypt = require('bcrypt');
+const LocalStrategy = require('passport-local').Strategy;
+const Users = require('../model.js');
 
-
-module.exports = function(passport) {
-    const checkPassword = function(password, savedPassword) {
-         return bcrypt.compareSync(password, savedPassword, (err, result) => {
-             return result;
-        });
-    }
-
-    passport.use('signin', new LocalStrategy(
-        {
-            usernameField: 'email',
-            passwordField: 'password'
-        },
-
-        function(email, password, cb) {
-            Users.findOne({ 'email': email }, (err, user) => {
-
-                if (err) {return cb(err); }
-
-                if (!user) { return cb(null, false); }
-                if (!checkPassword(password, user.password)) { return cb(null, false); }
-                return cb(null, user);
-
-            });
-
-        }));
-
-
-    passport.serializeUser(function(user, cb) {
-        cb(null, user._id);
+module.exports = (passport) => {
+  const checkPassword = (password, savedPassword) => {
+    return bcrypt.compareSync(password, savedPassword, (err, result) => {
+      return result;
     });
+  };
 
-
-    passport.deserializeUser(function(id, cb) {
-        Users.find({ '_id': id }, (err, user) => {
-            if (err) { return cb(err); }
-            cb(null, id);
-        });
+  passport.use('signin', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+  }, (email, password, cb) => {
+    Users.findOne({
+      email,
+    }, (err, user) => {
+      if (err) return cb(err);
+      if (!user) return cb(null, false);
+      if (!checkPassword(password, user.password)) return cb(null, false);
+      return cb(null, user);
     });
+  }));
+
+  passport.serializeUser((user, cb) => cb(null, user._id));
+
+  passport.deserializeUser((_id, cb) => {
+    Users.find({
+      _id,
+    }, (err) => {
+      if (err) {
+        return cb(err);
+      }
+      return cb(null, _id);
+    });
+  });
 };
