3629f6e078a589751ed17e0c283bea4d3e41681f
sdsautter <sdsautter@gmail.com>
2017-07-20 00:27:33 -0400

    Added logic to validate answer and then post

--- app/components/game-stage/gameInput.js
+++ app/components/game-stage/gameInput.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { addAnswer } from "../../utils/helpers";
 
 export default class GameInput extends Component {
    constructor() {
@@ -8,19 +10,60 @@ export default class GameInput extends Component {
     
     submitAnswer(event) {
         event.preventDefault();
-        const answer = {
-            answer: this.playerAnswer.value
-            // player: localStorage.getItem('username'),
+        
+        const answer = this.playerAnswer.value.trim();
+        const lettersArray = this.props.letters;
+
+        var answerArray = answer.split(" ");
+        console.log(answerArray);
+        console.log(lettersArray);
+        console.log(addAnswer(this.props.gameInstance));
+        //First we'll see if there's any time left
+        if (this.props.timeLeft > 0  && !alreadyAnswered) {
+            //We check to see if the answer array is the same length as the letter array
+            if (answerArray.length === letterArray.length) {
+                //If it is the same length then we run a for loop that is the length of the letter array
+                for (var i = 0; i < letterArray.length; i++) {
+                    //If the first letter in the player's answer starts with the corresponding, we push that answer to the database
+                    if (answerArray[i].startsWith(letterArray[i])) {
+                        //Run the score potential function first
+                        // scorePotential(roundNumber, timeLeft);
+                        alreadyAnswered = true;
+
+                        return helpers.addAnswer(this.props.gameInstanceId);
+                    } else {
+                        //If a letter doesn't start with the corresponding letter in array, return
+                        return alert(`Word ${i + 1} did not start with the corret letter`);
+                    }
+                }
+            } else if (answerArray.length > letterArray.length) {
+                alert("You have too many words. What are you trying to pull?");
+                return;
+            } else if (answerArray.length < letterArray.length) {
+                alert("You don't have enough words. What are you thinking?");
+                return;
+            }
         }
-        this.props.addAnswer(answer);
-        console.log(this.playerAnswer.value);
-        console.log("Answer submitted")
     }
 
     render() {
+        let alreadyAnswered = false;
+
         return(          
             <form className="input-group" onSubmit={this.submitAnswer}>
-                <input ref={(input) => {this.playerAnswer = input}} type="text" className="form-control" required placeholder="Type Answer Here" />
+                <input 
+                    ref={(input) => {this.playerAnswer = input}}
+                    name="answer" 
+                    type="text" 
+                    className="form-control" 
+                    required placeholder="Type Answer Here" 
+                />
+                <input 
+                    type="hidden" 
+                    name="playerId" 
+                    //We'll have to pull local player for this value
+                    value=""
+                />
                     <span className="input-group-btn">
                         <button id="answerSubmit" className="btn btn-secondary" type="button">Submit!</button>
                     </span>
