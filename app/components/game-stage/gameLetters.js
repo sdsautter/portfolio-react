50a66c586a43986607cde131491200cebdbaeef9
Scott Sautter <sdsautter@gmail.com>
2017-07-21 09:31:54 -0400

    Update gameLetters.js

--- app/components/game-stage/gameLetters.js
+++ app/components/game-stage/gameLetters.js
@@ -1,11 +1,20 @@
 import React, {Component} from "react";
 
 export default class GameLetters extends Component{
+    constructor() {
+        super();
+        this.stringLetters = this.stringLetters.bind(this);
+    }
+    
+    stringLetters() {
+     return this.props.letters.join(" ");   
+    }
+    
     render() {
         return (
             <div className="row text-center">
                 <div className="col-6 offset-4">
-                    <p className="game-letters">{this.props.letters}</p>
+                    <p className="game-letters">{this.stringLetters}</p>
                 </div>
             </div>
         )
