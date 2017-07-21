23edfc2825bdc27994321c7076536cf72a9e6fb8
Phil Sautter <psautter@gmail.com>
2017-07-21 17:41:38 -0400

    added vote controller and functions

---
+++ game/vote/controller.js
@@ -0,0 +1,27 @@
+const Vote = require('./model');
+
+exports.submitVote = async(round, player, answer) => {
+  const newVote = await new Vote({
+    round,
+    player,
+    answer,
+  }).save();
+  return newVote;
+};
+
+exports.findVotesByAnswer = async(answer) => {
+  const vote = await Vote.find({
+    answer,
+  });
+  if (vote.length === 0) return false;
+  return vote;
+};
+
+exports.findVotesByRoundAndPlayer = async(round, player) => {
+  const vote = await Vote.find({
+    round,
+    player,
+  });
+  if (vote.length === 0) return false;
+  return vote;
+};
