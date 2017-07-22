bab09f1ec57fb3a3c06be74983cb94f552bcc529
sdsautter <sdsautter@gmail.com>
2017-07-22 08:43:37 -0400

    Added commented out loops we'll need

--- app/components/results-stage/ResultsStage.js
+++ app/components/results-stage/ResultsStage.js
@@ -5,6 +5,16 @@ export default class ResultsStage extends Component {
     render() {
         return (
             <div className="col-10 main-game text-center">
+
+            {/* THIS INFO WILL NEED TO BE UPDATED WITH WHAT WE ACTUALLY GET
+                Object.keys(this.props.votingAnswers).map((key) => {
+                    let currentAnswer = this.props.votingAnswers[key];
+                    return (<ResultItem 
+                        answer={currentAnswer.answer} 
+                        username={currentAnswer.username} 
+                        />)
+                    })
+            */}
                 <ResultItem answer="Ginger Under Burns" username="Scott" votes="3" points="1000" />
                 <ResultItem answer="Gutter Utter Butter" username="Phil" votes="0" points="0" />
                 <ResultItem answer="Get Used Bitches" username="Byron" votes="1" points="150" />
