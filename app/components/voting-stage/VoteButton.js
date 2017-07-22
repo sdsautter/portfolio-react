5b69c1f611cb52ad8a73a0e1561b8b01bd2df612
sdsautter <sdsautter@gmail.com>
2017-07-22 13:43:58 -0400

    Should show answers. Can't click

--- app/components/voting-stage/VoteButton.js
+++ app/components/voting-stage/VoteButton.js
@@ -1,12 +1,45 @@
 import React, {Component} from "react";
 
 export default class VoteButton extends Component {
+    constructor() {
+       super();
+       this.voteAnswerPost = this.voteAnswerPost.bind(this);
+   }
+
+    voteAnswerPost(event) {
+        console.log("click!"); 
+        let vote = this.props.answerId;
+
+            // axios.post(`/api/games/${this.props.gameInstanceId}`, { vote })
+            // .then((response) => {
+            //     console.log("Voted");
+            // })
+            // .catch(function (error) {
+            //     console.log(error);
+            // });
+        
+
+    }
+
     render() {
         return (
 
-            <form action="" method="post">
-                <button name="answer" className="btn btn-warning vote-answer" value="">{this.props.answer}</button>
+
+            <form onSubmit={this.voteAnswerPost}>
+                <button 
+                    name="findGame"
+                    className="btn btn-success vote-answer"
+                >
+                    {this.props.answer}
+                </button>
             </form>
+
+
+            // <form onSubmit={this.voteAnswerPost}>
+            //     <input name="findGame" type="submit" value={this.props.answer} />
+            // </form>
+
+           
         )
     }
 }
