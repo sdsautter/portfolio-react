82a17c956f04ebcfc83013a5615922c8b9aa9232
Phil Sautter <psautter@gmail.com>
2017-07-22 15:57:47 -0400

    started game instance point calculation

--- game/gameEngine.js
+++ game/gameEngine.js
@@ -28,13 +28,6 @@ exports.submitAnswerOrVote = async(req, res) => {
 exports.getStatus = async(req, res, next) => {
   // Get the game instance Id from the URI
   const gameInstanceId = req.params.gameInstance;
-  // Find the game instance document
-  // const gameInstanceReference = await GameInstance.findById(gameInstanceId);
-  // console.log(gameInstanceReference);
-  // // Construct the status
-  // const gameStatus = new statusController.GameStatus(gameInstanceReference);
-  // // return the game status report
-  // const status = gameStatus.report;
 
   const status = await statusController.generateStatus(gameInstanceId);
   res.json(status);
@@ -45,49 +38,28 @@ const createTimer = (time) => {
   return new Promise((resolve) => {
     setTimeout(resolve, time);
   });
-  // var timeLapsed = 0;
-
-  // function checkTimeout() {
-  //   console.log(timeLapsed);
-  //   if (timeLapsed === time) return callback(timeLapsed);
-  //   setTimeout(() => {
-  //     timeLapsed++;
-  //     checkTimeout();
-  //   }, 1000);
-  // }
-
-  // return checkTimeout();
 };
 
 exports.startGame = async(gameInstanceId) => {
   for (let i = 1; i < 7; i++) {
     // create a new round for the game
     let newRound = await roundController.createRound(gameInstanceId);
-
+    
     if (newRound.error) return newRound.error;
     // Start the round timer for 60 seconds
-    // let allPlayersAnswered = false;
-    // var timeLapsed = 0;
-    // while (timeLapsed > 60) {
-    //   setInterval(function() {
-    //     console.log(timeLapsed);
-    //     timeLapsed++;
-    //   }, 1000);
-    //   // allPlayersAnswered = await getRoundStatus(newRound._id);
-    // }
     const playTimer = await createTimer(30);
-
     // Set the round state to voting
     const roundInVotingState = await roundController.setRoundState(newRound._id, 'voting');
     // After 30 seconds move to the results state
     const voteTimer = await createTimer(30);
-    // calculate the results
+    // calculate the results for the round
     await roundController.calculatePoints(newRound._id);
     // Set the round state to results
     const roundInResultsState = await roundController.setRoundState(newRound._id, 'results');
     const resultsTimer = await createTimer(15);
     // After 15 seconds move to the complete state
-
+    // add the points for the round to the game instance
+    await roundController.addPointToGameInstance(newRound._id);
     // Set the round state to complete
     const roundInCompleteState = await roundController.setRoundState(newRound._id, 'complete');
   }
