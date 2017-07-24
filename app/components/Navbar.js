10a0153ec4e7eba3e525c86c74dc4f56d2dd9ac3
sdsautter <sdsautter@gmail.com>
2017-07-24 16:35:04 -0400

    Added quick navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+
+export default class Navbar extends Component{
+    constructor() {
+        super();
+    }
+    
+    render() {
+        return (
+            <nav className="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+                <a className="navbar-brand" href="#">Acronauts</a>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-item nav-link" href="/logout">Logout</a>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
